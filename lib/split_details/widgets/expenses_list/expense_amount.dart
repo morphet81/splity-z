e23392eb3bd55f23b0bd0ffd671d3520140a3f58
Alexandre Obellianne <alexandre.obellianne@outlook.com>
2024-07-08 21:59:22 +0800

    Fix locale info not available on web

--- lib/split_details/widgets/expenses_list/expense_amount.dart
+++ lib/split_details/widgets/expenses_list/expense_amount.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:splity_z/shared/models/models.dart';
 import 'package:splity_z/shared/widgets/editable_content_pill.dart';
+import 'package:universal_io/io.dart';
 
 class ExpenseAmount extends StatelessWidget {
   const ExpenseAmount({super.key, required this.expense, required this.editOnRendered, required this.onAmountChanged});
