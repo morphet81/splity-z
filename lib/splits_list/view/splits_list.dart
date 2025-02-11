962e92c0a09685bd1abb5b54cdc859240f115e7e
Alexandre Obellianne <11921709+morphet81@users.noreply.github.com>
2025-02-11 15:49:43 +0800

    Allow to delete a split

--- lib/splits_list/view/splits_list.dart
+++ lib/splits_list/view/splits_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:splity_z/shared/bloc/split_bloc.dart';
 import 'package:splity_z/shared/models/split_state.dart';
+import 'package:splity_z/shared/widgets/split_bloc_undo_snack_bar.dart';
 import 'package:splity_z/splits_list/widgets/splits_list_item.dart';
 
 class SplitsList extends StatefulWidget {
@@ -14,6 +15,11 @@ class SplitsList extends StatefulWidget {
 }
 
 class _SplitsListState extends State<SplitsList> {
+  void handleDeleteSplit() {
+    final snackBar = SplitBlocUndoSnackBar(context: context);
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SplitBloc, SplitState>(
@@ -24,6 +30,7 @@ class _SplitsListState extends State<SplitsList> {
               return SplitsListItem(
                 isInEditMode: widget.isInEditMode,
                 split: state.splits[index],
+                onDelete: handleDeleteSplit,
               );
             },
             separatorBuilder: (context, index) {
--- lib/splits_list/widgets/splits_list_item.dart
+++ lib/splits_list/widgets/splits_list_item.dart
@@ -10,17 +10,33 @@ class SplitsListItem extends StatefulWidget {
   const SplitsListItem({
     required this.isInEditMode,
     required this.split,
+    required this.onDelete,
     super.key,
   });
 
   final Split split;
   final bool isInEditMode;
+  final VoidCallback onDelete;
 
   @override
   State<SplitsListItem> createState() => _SplitsListItemState();
 }
 
 class _SplitsListItemState extends State<SplitsListItem> {
+  void deleteSplit() {
+    context.read<SplitBloc>().add(
+          DeleteSplit(
+            split: widget.split,
+          ),
+        );
+
+    widget.onDelete();
+  }
+
+  void handleDeleteClick() {
+    deleteSplit();
+  }
+
   void handleTap() {
     // context.go('/split/${widget.split.id}');
     context.push('/split/${widget.split.id}');
@@ -40,7 +56,7 @@ class _SplitsListItemState extends State<SplitsListItem> {
             key: Key(widget.split.name),
             isInEditMode: widget.isInEditMode,
             showBorder: false,
-            onDelete: () => debugPrint('on delete'),
+            onDelete: handleDeleteClick,
             onTap: handleTap,
             child: Container(
               color: Theme.of(context).cardColor,
